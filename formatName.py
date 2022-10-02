1547c5a0116795bd3b0d5130f1846ffd4bc56b12
samgonz <doomteam1@gmail.com>
2022-10-01 22:32:56 -0500

    Added a check to make sure name is filled out

--- formatName.py
+++ formatName.py
@@ -1,3 +1,6 @@
 #Formats the users first and last name to be capitalized
 def formatName(f_name, l_name):
-    return f_name.title(), l_name.title()
+    if f_name == '' or l_name == '':
+        return 'First and last name must have a value.'
+    else:
+        return (f'The formated name is now {f_name.title()} {l_name.title()}.')
