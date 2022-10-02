57b4729a98126ef5d4c839ba18add858b9a01a85
samgonz <doomteam1@gmail.com>
2022-10-01 22:25:14 -0500

    initial commit

---
+++ formatName.py
@@ -0,0 +1,3 @@
+#Formats the users first and last name to be capitalized
+def formatName(f_name, l_name):
+    return f_name.title(), l_name.title()
--- nameFormater.py
+++ nameFormater.py
@@ -1,1 +1,11 @@
+import formatName
 
+#Gathers the first and last name of the user.
+f_name = input('What is your first name?')
+l_name = input('What is your last name?')
+
+#Formats the user name so the first letter of each name is capitalized
+f_name, l_name = formatName.formatName(f_name, l_name)
+
+#Prints the formated first and last name
+print(f'The formated name is now {f_name} {l_name}.')
